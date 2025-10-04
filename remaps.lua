427eef599e903e28f7739908e47c25d3cf3710ab
Arjun Gore <arjun.c.gore@gmail.com>
2025-10-04 03:01:39 -0400

    separated remaps (more to be separated in the future)

---
+++ remaps.lua
@@ -0,0 +1,30 @@
+return {
+remapped_kb = {
+	-- Add any playing remaps here
+
+	["MB4"] = "F3",
+	["MB5"] = "F6",
+	["F6"] = "MB5",
+	["LEFTALT"] = "LEFTCTRL",
+	["T"] = "BACKSPACE",
+	["BACKSPACE"] = "T",
+	["A"] = "K",
+	["Z"] = "A",
+	["K"] = "CAPSLOCK",
+	["O"] = "Q",
+	["Q"] = "O",
+	["D"] = "X",
+	["X"] = "RIGHTSHIFT",
+	["RIGHTSHIFT"] = "D",
+	["F1"] = "I",
+	["I"] = "F1",
+	["CAPSLOCK"] = "Z",
+},
+
+normal_kb = {
+	-- Add any remaps you want when disabling normal remaps (not necessary)
+
+	["A"] = "X",
+},
+
+}
