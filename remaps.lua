bec8f67bacea7f15246695c7c9ffc652ef857e43
Arjun Gore <arjun.c.gore@gmail.com>
2025-10-26 03:07:25 -0400

    cleanup

--- remaps.lua
+++ remaps.lua
@@ -1,11 +1,33 @@
 return {
-remapped_kb = {
-	-- Add any playing remaps here
-},
+    remapped_kb = {
+        -- Add any playing remaps here
+        ["MB4"] = "F3",
+        ["MB5"] = "F6",
+        ["F6"] = "MB5",
+        ["LEFTALT"] = "LEFTCTRL",
 
-normal_kb = {
-	-- Add any remaps you want to keep when disabling normal remaps (not necessary)
+        ["T"] = "BACKSPACE",
+        ["BACKSPACE"] = "T",
+        ["A"] = "K",
+        ["Z"] = "A",
+        ["K"] = "CAPSLOCK",
+        ["O"] = "Q",
+        ["Q"] = "O",
+        ["D"] = "X",
+        ["X"] = "RIGHTSHIFT",
+        ["RIGHTSHIFT"] = "D",
+        ["F1"] = "I",
+        ["I"] = "F1",
+        ["CAPSLOCK"] = "Z",
 
-},
+    },
+
+    normal_kb = {
+        -- Add any remaps you want to keep when disabling normal remaps (not necessary)
+        ["MB4"] = "F3",
+        ["MB5"] = "F6",
+        ["F6"] = "MB5",
+        ["LEFTALT"] = "LEFTCTRL",
+    },
 
 }
