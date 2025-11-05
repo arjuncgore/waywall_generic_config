13aa9adfa36cca1d764b45f38c0292f739d394bc
Arjun Gore <arjun.c.gore@gmail.com>
2025-11-05 01:20:12 -0500

    migrating personal config

--- config.lua
+++ config.lua
@@ -2,22 +2,23 @@
 
 -- ==== LOOKS ====
 local bg_col = "#000000"
-local toggle_bg_picture = false
-local primary_col = "#ec6e4e"
-local secondary_col = "#E446C4"
+local toggle_bg_picture = true
+local primary_col = "#272420"
+local secondary_col = "#877665"
+
 
 local ninbot_anchor = "topright" -- topleft, top, topright, left, right, bottomleft, bottomright
 local ninbot_opacity = 1         -- 0 to 1
 
 
 -- ==== MIRRORS ====
-local e_count = { enabled = true, x = 1340, y = 300, size = 5, colorkey = false }
-local thin_pie = { enabled = true, x = 1250, y = 500, size = 4, colorkey = false } -- Turning off colorkeying also maintains the original pie chart's dimensions and shows the percentages
-local thin_percent = { enabled = false, x = 1300, y = 850, size = 6 }
-local tall_pie = { enabled = true, x = 1250, y = 500, size = 4, colorkey = false } -- Leave same as thin for seamlessness
-local tall_percent = { enabled = false, x = 1300, y = 850, size = 6 }              -- Leave same as thin for seamlessness
+local e_count = { enabled = true, x = 1500, y = 400, size = 5, colorkey = true }
+local thin_pie = { enabled = true, x = 1490, y = 645, size = 4, colorkey = true } -- Turning off colorkeying also maintains the original pie chart's dimensions and shows the percentages
+local thin_percent = { enabled = true, x = 1568, y = 1050, size = 6 }
+local tall_pie = { enabled = true, x = 1490, y = 645, size = 4, colorkey = true } -- Leave same as thin for seamlessness
+local tall_percent = { enabled = true, x = 1568, y = 1050, size = 6 }             -- Leave same as thin for seamlessness
 
-local stretched_measure = false
+local stretched_measure = true
 
 
 -- ==== KEYBINDS ====
@@ -36,8 +37,8 @@ local toggle_remaps_key = "Insert"
 
 
 -- ==== MISC ====
-local remaps_text_config = { text = "rebinds off", x = 100, y = 100, size = 2 }
-local res_1440 = false
+local remaps_text_config = { text = "REBINDS OFF", x = 30, y = 100, size = 3 }
+local res_1440 = true
 local sens_change = { enabled = false, normal = 1.0, tall = 0.1 } -- make sure raw input is off
 
 
--- remaps.lua
+++ remaps.lua
@@ -2,10 +2,43 @@ return {
     remapped_kb = {
         -- Add any playing remaps here
 
+        ["MB4"] = "F3",
+        ["F6"] = "MB5",
+        ["LEFTALT"] = "LEFTCTRL",
+
+        -- ["MB5"] = "Home",
+        -- ["Q"] = "O",
+        -- ["A"] = "N",
+        -- ["D"] = "X" ,
+        -- ["CAPSLOCK"] = "0",
+        -- ["X"] = "RIGHTSHIFT",
+        -- ["O"] = "Q",
+
+        ["MB5"] = "Home",
+        ["T"] = "BACKSPACE",
+        ["BACKSPACE"] = "T",
+        ["A"] = "K",
+        ["Z"] = "A",
+        ["K"] = "CAPSLOCK",
+        ["O"] = "Q",
+        ["Q"] = "O",
+        ["D"] = "X",
+        ["X"] = "RIGHTSHIFT",
+        ["RIGHTSHIFT"] = "D",
+        ["F1"] = "I",
+        ["I"] = "F1",
+        ["CAPSLOCK"] = "0",
+        ["0"] = "Z",
+
     },
 
     normal_kb = {
         -- Add any remaps you want to keep when disabling normal remaps (not necessary)
+
+        ["MB4"] = "F3",
+        ["MB5"] = "F6",
+        ["F6"] = "MB5",
+        ["LEFTALT"] = "LEFTCTRL",
 
     },
 
