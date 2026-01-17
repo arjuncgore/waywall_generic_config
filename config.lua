0b41739469e902b7c4bd0cd876319b658078dff5
Arjun Gore <arjun.c.gore@gmail.com>
2026-01-17 10:43:28 -0500

    fixed default colors

--- config.lua
+++ config.lua
@@ -5,9 +5,8 @@ return {
     toggle_bg_picture = false,
     text_col = "#FFFFFF",
     pie_chart_1 = "#EC6E4E",
-    pie_chart_2 = "#E446C4",
-    pie_chart_3 = "#46CE66",
-
+    pie_chart_2 = "#46CE66",
+    pie_chart_3 = "#E446C4",
 
     ninbot_anchor = "topright", -- topleft, top, topright, left, right, bottomleft, bottomright
     ninbot_opacity = 1,         -- 0 to 1
@@ -15,9 +14,9 @@ return {
 
     -- ==== MIRRORS ====
     e_count = { enabled = true, x = 1340, y = 300, size = 5, colorkey = false },
-    thin_pie = { enabled = true, x = 1250, y = 500, size = 4, colorkey = false }, -- Turning off colorkeying also maintains the original pie chart's dimensions and shows the percentages
+    thin_pie = { enabled = true, x = 1200, y = 400, size = 4, colorkey = false }, -- Turning off colorkeying also maintains the original pie chart's dimensions and shows the percentages
     thin_percent = { enabled = false, x = 1300, y = 850, size = 6 },
-    tall_pie = { enabled = true, x = 1250, y = 500, size = 4, colorkey = false }, -- Leave same as thin for seamlessness
+    tall_pie = { enabled = true, x = 1200, y = 400, size = 4, colorkey = false }, -- Leave same as thin for seamlessness
     tall_percent = { enabled = false, x = 1300, y = 850, size = 6 },              -- Leave same as thin for seamlessness
 
     stretched_measure = false,
@@ -40,7 +39,8 @@ return {
 
     -- ==== MISC ====
     remaps_config = { layout_name = "mc", enabled = false }, -- ~/.config/xkb/symbols/mc
-    remaps_text_config = { text = "remaps off", x = 100, y = 100, size = 2 },
+    remaps_text_config = { text = "remaps off", x = 100, y = 100, size = 2, color = "#000000" },
+
     res_1440 = false,
     sens_change = { enabled = false, normal = 1.0, tall = 0.1 }, -- make sure raw input is off
     enable_resize_animations = false,
